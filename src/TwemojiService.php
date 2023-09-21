b7e901a1d795f46cdc72d9d4d02306ff47481796
Pierre-Yves <pyrrahnas@gmail.com>
2023-09-21 19:30:56 +0200

    Update TwemojiService.php

--- src/TwemojiService.php
+++ src/TwemojiService.php
@@ -14,7 +14,7 @@ final class TwemojiService
         HttpClientInterface $httpClient,
         CacheInterface $cache,
         $format = 'svg',
-        $version = 'latest'
+        $version = '14.0.2'
     )
     {
         $this->emojis = $cache->get('twemoji', function (CacheItem $item) use ($httpClient, $format, $version) {
